a06e89afa1b471a017ea538f196696c3521789ec
hemwatie <hemwatiecodes@gmail.com>
2021-06-12 19:43:01 +0000

    Added highlighted results to the innerHTML text

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-
+    //Initalizes variables, and assigns their entered values. 
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years *  rate / 100;
 
+    //Calculates new year for expected interest.
     var year = new Date().getFullYear()+parseInt(years);
 
+    //Checks that the principal entered is a positive number.
     if(principal<1)
     {
+
         document.getElementById("result").innerHTML="Enter a positive number";
     }
     else
     {
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
+    document.getElementById("result").innerHTML=
+    "If you deposit <mark>"
+    +principal+
+    "</mark>,\<br\>at an interest rate of <mark>"
+    +rate+
+    "%</mark>,\<br\>You will receive an amount of <mark>"
+    +interest+
+    "</mark>,\<br\>in the year <mark>"
+    +year+
+    "</mark>,\<br\>";
     }
 }
 
 function  updateRate()
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText  = rateval;
+    document.getElementById("rate_val").innerText  = rateval+" %";
     
 }
 
